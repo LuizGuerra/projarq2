c3882a761df2df870ff0ead1b1bbeff42bfeee59
LuizGuerra <luizpedrofg@gmail.com>
2020-12-04 14:18:36 -0300

    Order methods

--- src/src/model/Order.java
+++ src/src/model/Order.java
@@ -3,69 +3,107 @@ package src.model;
 import java.util.Date;
 import java.util.Objects;
 
+// package
 public class Order {
+    private String store;
     private OrderStatus status;
     private Item item;
     private Integer quantity;
-    private Date paymentDay, outForDelivery, delivered;
+    private Date deliveryForecast, paymentDay, outForDelivery, delivered;
 
-    public Order(OrderStatus status, Item item, Integer quantity) {
+    public Order(String store, OrderStatus status, Item item, Integer quantity, Date deliveryForecast) {
+        this.store = store;
+        this.deliveryForecast = deliveryForecast;
         this.status = status;
         this.item = item;
         this.quantity = quantity;
+    }
+
+    public Order(String store, OrderStatus status, Item item, Integer quantity, Date deliveryForecast, Date paymentDay) {
+        this.store = store;
+        this.status = status;
+        this.item = item;
+        this.quantity = quantity;
+        this.deliveryForecast = deliveryForecast;
+        this.paymentDay = paymentDay;
+    }
+
+    public Order(String store, OrderStatus status, Item item, Integer quantity, Date deliveryForecast, Date paymentDay, Date outForDelivery) {
+        this.store = store;
+        this.status = status;
+        this.item = item;
+        this.quantity = quantity;
+        this.deliveryForecast = deliveryForecast;
+        this.paymentDay = paymentDay;
+        this.outForDelivery = outForDelivery;
+    }
+
+    public Order(String store, OrderStatus status, Item item, Integer quantity, Date deliveryForecast, Date paymentDay, Date outForDelivery, Date delivered) {
+        this.store = store;
+        this.status = status;
+        this.item = item;
+        this.quantity = quantity;
+        this.deliveryForecast = deliveryForecast;
+        this.paymentDay = paymentDay;
+        this.outForDelivery = outForDelivery;
+        this.delivered = delivered;
+    }
+
+    public String getStore() {
+        return store;
     }
 
     public OrderStatus getStatus() {
         return status;
     }
 
-    public void setStatus(OrderStatus status) {
-        this.status = status;
-    }
-
     public Item getItem() {
         return item;
-    }
-
-    public void setItem(Item item) {
-        this.item = item;
     }
 
     public Integer getQuantity() {
         return quantity;
     }
 
-    public void setQuantity(Integer quantity) {
-        this.quantity = quantity;
-    }
-
     public Date getPaymentDay() {
         return paymentDay;
-    }
-
-    public void setPaymentDay(Date paymentDay) {
-        this.paymentDay = paymentDay;
     }
 
     public Date getOutForDelivery() {
         return outForDelivery;
     }
 
-    public void setOutForDelivery(Date outForDelivery) {
-        this.outForDelivery = outForDelivery;
-    }
-
     public Date getDelivered() {
         return delivered;
+    }
+
+    public void setStatus(OrderStatus status) {
+        this.status = status;
+    }
+
+    public void setPaymentDay(Date paymentDay) {
+        this.paymentDay = paymentDay;
+    }
+
+    public void setOutForDelivery(Date outForDelivery) {
+        this.outForDelivery = outForDelivery;
     }
 
     public void setDelivered(Date delivered) {
         this.delivered = delivered;
     }
 
+    public Date getDeliveryForecast() {
+        return deliveryForecast;
+    }
+
+    public void setDeliveryForecast(Date deliveryForecast) {
+        this.deliveryForecast = deliveryForecast;
+    }
+
     @Override
     public String toString() {
-        return "Item " + item.getName() + ", with status " + status;
+        return "Item { " + item.getName() + ", with status " + status + " }";
     }
 
     @Override
